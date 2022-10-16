2923879f0d4fe16009c75d9be55bc1949d8bde08
Oğuzhan Yılmaz <93767408+ouzymz@users.noreply.github.com>
2022-10-16 20:00:32 +0300

    Add files via upload

---
+++ CSharpHazir/01_BuiltInMethods/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using System.Reflection;
+
+string metin = "Zafer ";
+
+
+int index = metin.IndexOf('r');
+
+Console.WriteLine("IndexOf : " + index);
---
+++ Diziler/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int sayac = 0;
+
+while (sayac < students2.Length)
+{
+    Console.WriteLine(String.Format(students2[counter]);
+    sayac++;    
+}
+
+#region DoWhile ile
+
+int counter = 0;
+
+do
+{
+    Console.WriteLine(students2[counter]);
+    counter++;
+
+} while (counter < students2.Length);
+
+
+
+
+#endregion
+
+Array.Sort(students2); //harflere göre alfabatik sıralır
+
+foreach (var item in students2)
+{
+    Console.WriteLine(item);
+}
+
